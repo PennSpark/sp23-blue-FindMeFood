6ea4e4354c2893473551ab1a5fda0222a327f8d6
kirby1926 <bakj@seas.upenn.edu>
2023-03-19 15:47:25 -0400

    resized to fit any screen

--- Components/Styles.js
+++ Components/Styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native"
 export default StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'rgba(39, 37, 53, 1.0)',
+      backgroundColor: 'rgba(42, 40, 57, 1.0)',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -12,43 +12,43 @@ export default StyleSheet.create({
       left: 0,
       right: 0,
       top: 0,
-      height: 850,
+      height: "100%",
     },
   
     nav: {
       position: 'absolute',
       left: 0,
       right: 0,
-      top: 715,
-      height: 800,
+      top: "84.6%",
+      height: "50%",
       backgroundColor: 'rgba(59, 57, 73, 1.0)',
       borderRadius: 25,
       alignItems: 'center',
     },
     nav_flexbox: {
-      width: 500,
-      height: 110,
+      width: "120%",
+      height: "27.5%",
       position: 'flex',
       flexDirection: 'row',
       alignItems: 'center' ,
       justifyContent: 'space-evenly'
     },
     home_background: {
-      width: 100,
-      height: 70,
+      width: "21%",
+      height: "64%",
       position: 'flex',
       alignItems: 'center',
       backgroundColor: 'rgba(89, 87, 103, 0.8)',
-      borderRadius: 15
+      borderRadius: 15,
     },
 
     text: {
       backgroundColor: 'transparent',
-      fontSize: 15,
+      fontSize: "18%",
       color: '#fff',
     },
     button: {
       backgroundColor: 'pink',
-      width: 100
+      width: "20%"
     },
 })
